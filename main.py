05a309ad506c90205d3080b5f22c67ae53ebd42f
Milad Alkhamis <mi.alkhamis@pm.me>
2021-08-06 16:16:08 +0430

    add cmd tester

--- main.py
+++ main.py
@@ -7,7 +7,6 @@ from time import sleep
 
 
 
-
 def input_check():
     """
     This function check input variables
@@ -24,27 +23,52 @@ def input_check():
     
     if str(DWS_TESTER_THRESHOLD).isnumeric():
         DWS_TESTER_THRESHOLD = int(DWS_TESTER_THRESHOLD)
-        # print(DWS_TESTER_THRESHOLD)
+        
     else:
+
         print("Please input a number for DWS_TESTER_THRESHOLD \n")
         return 1
     
     if str(DWS_TESTER_INTERVAL).isnumeric():
         DWS_TESTER_INTERVAL = int(DWS_TESTER_INTERVAL)
-        # print(DWS_TESTER_INTERVAL)
+        
     else:
+
         print("Please input a number for DWS_TESTER_INTERVAL \n")
         return 1
     
     return (DWS_TESTER_COMMAND,DWS_TESTER_THRESHOLD,DWS_TESTER_INTERVAL)
 
 
+def cmd_tester(args):
 
-
+    command,threshold,interval = args
+    while threshold  > 0 :
+        run_code = run(["python" , command], stdout = stdout, stderr = stderr)
+        threshold = threshold - 1
+        if run_code.returncode == 0 :
+            print ("code run successfully")
+            return 1
+        else:
+            if threshold == 0:
+                return 1
+            else:
+                sleep(interval)
+            
+    return 1
+        
 if __name__ == '__main__':
     check_result = input_check()
     if check_result == 1:
         print("please check your inputs and try agian")
         exit(1)
     else:
-        pass
+        cmd_result =  cmd_tester(check_result)
+        # print(f"status code: {status}")
+        if cmd_result == 0:
+            print("Program runs successfully")
+            exit(0)
+        else:
+            print("Program failed", file = stderr)
+            exit(1)
+    
